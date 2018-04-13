f90abf1cddb60b81c7721f227b78e19fe045c822
oscarlvp <oscarlvp@gmail.com>
2018-04-13 15:38:22 +0200

    Removed all commented code.

--- src/main/java/eu/stamp_project/mutationtest/descartes/DescartesEngineFactory.java
+++ src/main/java/eu/stamp_project/mutationtest/descartes/DescartesEngineFactory.java
@@ -64,60 +64,6 @@ public class DescartesEngineFactory implements MutationEngineFactory{
 
     }
 
-    /**
-     * Creates the engine instance from the given configuration
-     * @param mutateStaticInitializers
-     * @param excludedMethods
-     * @param loggingClasses
-     * @param mutators
-     * @param detectInLinedCode In our case this can be ignored
-     * @return
-     */
-    /*
-    public MutationEngine createEngine(final boolean mutateStaticInitializers,
-                                       final Predicate<String> excludedMethods,
-                                       final Collection<String> loggingClasses,
-                                       final Collection<String> mutators,
-                                       boolean detectInLinedCode) {
-        return new DescartesMutationEngine(getMethodFilter(mutateStaticInitializers, excludedMethods),
-                getLoggingClassesSet(loggingClasses), getMutationOperators(mutators));
-    }
-
-    private static Predicate<Method> getMethodFilter(final boolean mutateStaticInitializers, final Predicate<String> excludedMethods) {
-        //Only filters defined by the user. Other filters are applied in code.
-        return new Predicate<Method>() {
-            public Boolean apply(Method method) {
-                String name = method.getName();
-                return
-                       (name.equals("<clinit>") && !mutateStaticInitializers)
-                       || excludedMethods.apply(name);
-            }
-        };
-    }
-
-    private static Set<String> getLoggingClassesSet(Collection<String> loggingClasses) {
-        return new HashSet<String>(loggingClasses);
-    }
-
-    private static Collection<MutationOperator> getMutationOperators(Collection<String> mutators) {
-        LinkedList<MutationOperator> result = new LinkedList<MutationOperator>();
-        if(mutators.size() == 0) {
-            //Default operators. //TODO: If the list grows put in a separated method
-            result.add(VoidMutationOperator.getInstance());
-            result.add(NullMutationOperator.getInstance());
-            return result;
-        }
-        for (String id :
-                mutators) {
-            try {
-                result.add(MutationOperator.fromID(id));
-            }catch (WrongOperatorException exc) {
-                org.pitest.util.Log.getLogger().log(Level.WARNING, "Illegal ID value. Details: " + exc.getMessage());
-            }
-        }
-        return result;
-    }*/
-
     public String name() {
         return "descartes";
     }
