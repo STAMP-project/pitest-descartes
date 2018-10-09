b5eb39eb1ac4740e14e740be4d1ce00ff08dc00f
oscarlvp <oscarlvp@gmail.com>
2018-10-09 18:42:17 +0200

    Testing the "returns null" matcher

---
+++ src/test/java/eu/stamp_project/mutationtest/descartes/stopmethods/ReturnsNullMatcherTest.java
@@ -0,0 +1,16 @@
+package eu.stamp_project.mutationtest.descartes.stopmethods;
+
+import org.pitest.bytecode.analysis.MethodTree;
+
+public class ReturnsNullMatcherTest extends BaseMethodMatcherTest {
+    @Override
+    public boolean criterion(MethodTree method) {
+        return method.rawNode().name.equals("returnNull");
+
+    }
+
+    @Override
+    public StopMethodMatcher getMatcher() {
+        return StopMethodMatchers.returnsNull();
+    }
+}
