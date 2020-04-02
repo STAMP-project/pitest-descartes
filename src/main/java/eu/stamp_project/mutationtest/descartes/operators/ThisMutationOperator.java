18767f1583a36ae6467de8103ac9e315c92bb658
Ipsita <ipsita0911gmail.com>
2020-04-02 10:41:36 +0530

    Updated ThisMutator and Lexer

--- src/main/java/eu/stamp_project/mutationtest/descartes/operators/ThisMutationOperator.java
+++ src/main/java/eu/stamp_project/mutationtest/descartes/operators/ThisMutationOperator.java
@@ -7,22 +7,29 @@ import org.pitest.reloc.asm.Type;
 import org.pitest.reloc.asm.commons.Method;
 
 /**
- * Replaces the method body with a <code>return null</code> statement
+ * Replaces the method body with a <code>return this</code> statement
  */
 public class ThisMutationOperator extends MutationOperator{
 
     /**
      * Returns a value indicating whether the operator can transform the given method.
-     * In this case whether <code>null</code> could be assigned to the return type.
+     * In this case whether <code>this</code> could be assigned to the return type.
      *
      * @param method Method to be tested by the operator
-     * @return A boolean value indicating if null can be assigned to the return type
+     * @return A boolean value indicating if this can be assigned to the return type
      */
+
+    // @Override
+    // public boolean canMutate(Method method) {
+    //     return true;
+    // }
+
     @Override
     public boolean canMutate(ClassName className, Method method) {
         String[] objArr = method.getReturnType().toString().split("/");
         String[] classArr = className.asJavaName().split("\\.");
         int check = objArr[objArr.length - 1].indexOf(classArr[classArr.length - 1]);
+        
         if(check != -1){
             return  true;
         }
@@ -31,7 +38,7 @@ public class ThisMutationOperator extends MutationOperator{
 
     @Override
     public void generateCode(Method method, MethodVisitor mv) {
-        mv.visitInsn(Opcodes.ACONST_NULL);
+        mv.visitVarInsn(Opcodes.ALOAD, 0);
         mv.visitInsn(Opcodes.ARETURN);
     }
 
